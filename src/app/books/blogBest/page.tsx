93b71abad4f3fcaecd489cfd650aa578b10f7726
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-09-15 23:03:43 +0200

    get books by group

--- src/app/books/blogBest/page.tsx
+++ src/app/books/blogBest/page.tsx
@@ -1,5 +1,34 @@
-const blogBest = () => {
-  return <h1>여기에 blogBest가 보여집니다.</h1>;
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { getBooksByGroupRequest } from '@/app/actions/types';
+import GroupBooksContainer from '@/app/components/Book/GroupBooksContainer';
+import { RootState } from '@/app/reducers';
+import { AppDispatch } from '@/app/store/store';
+import { Container } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+
+const BlogBest = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const [page, setPage] = useState<number>(1);
+  const { groupBooks, pageSize, isGetBooksByGroupLoading } = useSelector((store: RootState) => store.book);
+
+  const handleSeeMore = () => {
+    setPage((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    dispatch(getBooksByGroupRequest({ groupName: 'BlogBest', page: page, pageSize: pageSize }));
+  }, [page]);
+
+  return (
+    <>
+      <Container data-testid="books-group-container" sx={{ width: '100vw', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+        <GroupBooksContainer books={groupBooks} title={'Blog Best Books'} handleSeeMore={handleSeeMore} isGetBooksByGroupLoading={isGetBooksByGroupLoading} />
+      </Container>
+    </>
+  );
 };
 
-export default blogBest;
+export default BlogBest;
--- src/app/reducers/book.ts
+++ src/app/reducers/book.ts
@@ -5,6 +5,9 @@ import {
   GET_BOOK_FAILURE,
   GET_BOOK_REQUEST,
   GET_BOOK_SUCCESS,
+  GET_BOOKS_BY_GROUP_FAILURE,
+  GET_BOOKS_BY_GROUP_REQUEST,
+  GET_BOOKS_BY_GROUP_SUCCESS,
 } from '../actions/constants';
 import { BookActionTypes } from '../actions/types';
 import { Book } from '../models/book';
@@ -12,25 +15,35 @@ import { Book } from '../models/book';
 type InitialState = {
   books: Book[];
   count: number;
+  groupBooks: Book[];
   isGetAllBooksLoading: boolean;
   isGetAllBooksDone: boolean;
   isGetAllBooksError: string;
+  isGetBooksByGroupLoading: boolean;
+  isGetBooksByGroupDone: boolean;
+  isGetBooksByGroupError: string;
   isGetBookLoading: boolean;
   isGetBookDone: boolean;
   isGetBookError: string;
   book: Book[];
+  pageSize: number;
 };
 
 export const initialState: InitialState = {
   books: [],
   count: 0,
+  groupBooks: [],
   isGetAllBooksLoading: false,
   isGetAllBooksDone: false,
   isGetAllBooksError: '',
+  isGetBooksByGroupLoading: false,
+  isGetBooksByGroupDone: false,
+  isGetBooksByGroupError: '',
   isGetBookLoading: false,
   isGetBookDone: false,
   isGetBookError: '',
   book: [],
+  pageSize: 20,
 };
 
 function bookReducer(state = initialState, action: BookActionTypes) {
@@ -41,6 +54,13 @@ function bookReducer(state = initialState, action: BookActionTypes) {
       return { ...state, isGetAllBooksLoading: false, isGetAllBooksDone: true, books: action.payload, count: action.count };
     case GET_ALL_BOOKS_FAILURE:
       return { ...state, isGetAllBooksLoading: false, isGetAllBooksDone: false, isGetAllBooksError: action.error };
+
+    case GET_BOOKS_BY_GROUP_REQUEST:
+      return { ...state, isGetBooksByGroupLoading: true };
+    case GET_BOOKS_BY_GROUP_SUCCESS:
+      return { ...state, isGetBooksByGroupLoading: false, isGetBooksByGroupDone: true, groupBooks: state.groupBooks.concat(action.payload) };
+    case GET_BOOKS_BY_GROUP_FAILURE:
+      return { ...state, isGetBooksByGroupLoading: false, isGetBooksByGroupDone: false, isGetBooksByGroupError: action.error };
 
     case GET_BOOK_REQUEST:
       return { ...state, isGetBookLoaing: true };
