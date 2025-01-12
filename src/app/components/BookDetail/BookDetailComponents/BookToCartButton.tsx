fd96253ec4efeaeac459ca61f02476ce6d96a558
Hwang Cheol <joel.c.hwang@gmail.com>
2025-01-12 09:28:51 +0900

    bookcard detail

--- src/app/components/BookDetail/BookDetailComponents/BookToCartButton.tsx
+++ src/app/components/BookDetail/BookDetailComponents/BookToCartButton.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import { useState } from 'react';
 
+import { toggleWishlistRequest } from '@/app/actions/types';
+import { AppDispatch } from '@/app/store/store';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { Button, Box } from '@mui/material';
 import { pink } from '@mui/material/colors';
 import { useRouter } from 'next/navigation';
+import { useDispatch } from 'react-redux';
 
 // Props 타입 정의
 
 interface Book {
+  id: number;
   title: string;
   author: string;
   priceStandard: number;
   cover: string;
   publisher: string;
+  isBookmarked: boolean;
 }
 
 interface BookToCartButtonProps {
@@ -22,7 +29,9 @@ interface BookToCartButtonProps {
 }
 
 const BookToCartButton: React.FC<BookToCartButtonProps> = ({ book, quantity }) => {
+  const dispatch = useDispatch<AppDispatch>();
   const router = useRouter();
+  const [isBookmarked, setIsBookmarked] = useState<boolean>(book.isBookmarked);
   const handleAddToCart = () => {
     // 카트에 추가하는 로직 구현
     const cartItem = { ...book, quantity };
@@ -35,8 +44,8 @@ const BookToCartButton: React.FC<BookToCartButtonProps> = ({ book, quantity }) =
 
   const handleWishlistClick = () => {
     // 찜하기 기능 구현
-    console.log('찜하기: ', book.title);
-    debugger;
+    dispatch(toggleWishlistRequest(book.id));
+    setIsBookmarked(!isBookmarked);
   };
 
   return (
@@ -47,7 +56,7 @@ const BookToCartButton: React.FC<BookToCartButtonProps> = ({ book, quantity }) =
       <Button
         variant="outlined"
         color="primary"
-        startIcon={<FavoriteBorderIcon sx={{ color: pink[500] }} />}
+        startIcon={isBookmarked ? <FavoriteIcon sx={{ color: pink[500] }} /> : <FavoriteBorderIcon sx={{ color: pink[500] }} />}
         sx={{ height: '60px', flexGrow: 1 }}
         onClick={handleWishlistClick}>
         찜하기
